b0ea285ea110f6d081888947afdc83021d9582cf
udin9 <61424293+udin9@users.noreply.github.com>
2026-01-19 18:02:08 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1258,9 +1258,10 @@ function generateReport() {
                     <tbody>
                         ${resA.rows}
                         <tr class="report-total-row">
-                            <td colspan="2" class="text-right">Jumlah Kuantiti / Jumlah</td>
+                            <td colspan="2" class="text-right">Jumlah Kuantiti</td>
                             <td class="text-center">${resA.totalQty}</td>
                             <td></td>
+                            <td colspan="2" class="text-right">Jumlah</td>
                             <td class="text-right">RM ${resA.totalPrice.toFixed(2)}</td>
                         </tr>
                     </tbody>
